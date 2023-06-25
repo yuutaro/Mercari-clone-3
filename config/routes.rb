46e50d38c6bd4e8f021e00469c4b0a4af1c807e4
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-06-25 14:10:27 +0900

    user用のコントローラー作成 ch18

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confrimations: 'users/confirmations',
+    passwords: 'users/passwords',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    unlocks: 'users/unlocks'
+  }
   root to: "home#index"
 end
