bf52f954200895434e8e36bc2761f8059d1b498e
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2023-06-29 13:00:02 +0900

    本人情報新規登録画面作成　I18n適応されず要修正 ch24.14

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -1,5 +1,9 @@
 class UserInformationsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    # has_oneアソシエーションの場合、current_userのidがあらかじめ代入されている
+    @user_information = current_user.build_user_information
   end
 
   def create
