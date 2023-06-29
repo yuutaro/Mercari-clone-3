3087df8da597c53d2f31788bc1a721e6e00d90fc
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2023-06-29 13:01:14 +0900

    user_infomationストロングパラメーター実装　ch24.15

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -7,5 +7,25 @@ class UserInformationsController < ApplicationController
   end
 
   def create
+    @user_information = current_user.build_user_information(user_information_params)
+    if @user_information.save
+      redirect_to root_path, notice: "ユーザー情報を登録しました"
+    else
+      flash.now[:alert] = "ユーザー情報の登録に失敗しました"
+      render :new
+    end
+  end
+
+  private
+
+  # ストロングパラメーター
+  def user_information_params
+    params.require(:user_information).permit(
+      :family_name,
+      :given_name,
+      :family_name_kana,
+      :given_name_kana,
+      :birth_date
+    )
   end
 end
