aafab82c31aae18c161627fe0996fe221c2e7b72
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2023-06-29 11:18:11 +0900

    新規登録後に本人登録画面に遷移するように修正 ch24.9

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,4 +59,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_sign_up_path_for(resource)
+    new_user_information_path
+  end
 end
