1e33b71f1dc589b317356db05289ee0cdb415bcb
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-06-30 18:58:55 +0900

    パスワード再設定完了メール送信設定変更

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -132,7 +132,8 @@ Devise.setup do |config|
   # config.send_email_changed_notification = false
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  # パスワード再設定時にメールを送信するかどうか
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
