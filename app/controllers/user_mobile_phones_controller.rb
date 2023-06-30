b6281c575773134b1fbb5110c07e6054b11524c8
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-06-30 16:27:41 +0900

    SMS認証アクション作成 ch25

--- app/controllers/user_mobile_phones_controller.rb
+++ app/controllers/user_mobile_phones_controller.rb
@@ -10,7 +10,7 @@ class UserMobilePhonesController < ApplicationController
 
     if @user_mobile_phone.save
       @user_mobile_phone.send_auth_code_to_sms
-      redirect_to verification_user_mobile_phones_path, notice: "電話番号登録に成功しました。"
+      redirect_to verification_user_mobile_phone_path, notice: "電話番号登録に成功しました。"
     else
       flash.now[:alert] = "電話番号登録に失敗しました。"
       render :new
@@ -18,6 +18,15 @@ class UserMobilePhonesController < ApplicationController
   end
 
   def verification
+    @user_mobile_phone = current_user.user_mobile_phone
+    return unless request.post?
+
+    if @user_mobile_phone.verify(params[:auth_code])
+      redirect_to root_path, notice: "電話番号の認証に成功しました。"
+    else
+      flash.now[:alert] = "認証コードが間違っています。"
+      render :verification
+    end
   end
 
   private
