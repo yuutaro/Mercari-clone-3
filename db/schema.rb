42922acb50607fbd0a42df7a76a2fa15c77e6e90
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-07-03 10:00:08 +0900

    マイグレーションファイルの書き忘れの修正？

--- db/schema.rb
+++ db/schema.rb
@@ -60,9 +60,10 @@ ActiveRecord::Schema.define(version: 2023_07_01_093016) do
   end
 
   create_table "shipping_day_types", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_shipping_day_types_on_name", unique: true
   end
 
   create_table "shipping_payer_types", force: :cascade do |t|
