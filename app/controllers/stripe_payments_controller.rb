dab2a50262b9f0b284ea2187ae5078fd99e28cd9
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-07-05 16:24:18 +0900

    stripe_paymentコントローラー作成 ch42.13

---
+++ app/controllers/stripe_payments_controller.rb
@@ -0,0 +1,26 @@
+class StripePaymentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
+  def index
+  end
+
+  def new
+    @setup_intent = Stripe::SetupIntent.create(
+      customer: current_user.stripe_customer.customer_id,
+      payment_method_types: ['card'],
+    )
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
