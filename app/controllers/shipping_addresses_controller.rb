17ce22966bb18d27c5a67bb24f1fcc5f9b19cbac
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-07-07 18:19:08 +0900

    shipping_addressコントローラー作成 ch45.7

---
+++ app/controllers/shipping_addresses_controller.rb
@@ -0,0 +1,29 @@
+class ShippingAddressesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:new]
+
+  def index
+  end
+
+  def new
+    @shipping_address = current_user.shipping_addresses.build
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
