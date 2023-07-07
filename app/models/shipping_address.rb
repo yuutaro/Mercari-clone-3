d4a9083ca29427c3a315c66cdf75b1b44b04124e
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-07-07 18:05:05 +0900

    hippingAddressバリデーション追加 ch45.4

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,4 +1,17 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture
+
+  ZENKAKU_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  ZENKAKU_KANA_REGEX =  /\A[ァ-ヶー－]+\z/
+
+  validates :family_name,        presence :true, format: { with: ZENKAKU_REGEX, message: '全角文字を使用してください' }, allow_blank: true
+  validates :given_name,         presence :true, format: { with: ZENKAKU_REGEX, message: '全角文字を使用してください' }, allow_blank: true
+  validates :family_name_kana,   presence :true, format: { with: ZENKAKU_KANA_REGEX, message: '全角カタカナを使用してください' }, allow_blank: true
+  validates :given_name_kana,    presence :true, format: { with: ZENKAKU_KANA_REGEX, message: '全角カタカナを使用してください' }, allow_blank: true
+  validates :postal_code,        presence :true
+  validates :city,               presence :true
+  validates :line,               presence :true
+  validates :building_name,      presence :true
+  validates :phone_number,       presence :true
 end
