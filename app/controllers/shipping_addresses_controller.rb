81daee3eacadb5ca94d1dc3e0b70047715c983c9
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-07-07 19:45:42 +0900

    shipping_addressCreateアクション作成 ch45.10

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -10,6 +10,13 @@ class ShippingAddressesController < ApplicationController
   end
 
   def create
+    @shipping_address = current_user.shipping_addresses.build(shipping_address_params)
+    if @shipping_address.save
+      redirect_to item_path(@shipping_address.item), notice: '配送先を登録しました'
+    else
+      flash.now[:alert] = '配送先の登録に失敗しました'
+      render :new
+    end
   end
 
   def edit
@@ -25,5 +32,20 @@ class ShippingAddressesController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(
+      :family_name,
+      :given_name,
+      :family_name_kana,
+      :given_name_kana,
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :line,
+      :building_name,
+      :phone_number
+    )
   end
 end
