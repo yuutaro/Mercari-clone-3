03277d4855838668c0526ab1128dfba7531d4d13
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-07 18:52:23 +0900

    41.13 通報用routingの追加

--- config/routes.rb
+++ config/routes.rb
@@ -31,11 +31,15 @@ Rails.application.routes.draw do
 
   # item_favorites_path POST   /items/:item_id/favorites(.:format)  favorites#create
   # item_favorite_path  DELETE /items/:item_id/favorites(.:format)  favorites#destroy
-  # item_comments_path  POST   /items/:item_id/comments(.:format)   comments#create
-  # comment_path        DELETE /comments/:id(.:format)              comments#destroy
   resources :items do
     resources :favorites, only: %i[create destroy], param: :item_id, shallow: true
-    resources :comments, only: %i[create destroy], shallow: true
+    # item_comments_path  POST   /items/:item_id/comments(.:format)   comments#create
+    # comment_path        DELETE /comments/:id(.:format)              comments#destroy
+    resources :comments, only: %i[create destroy], shallow: true do
+      # comment_reports_path    POST /comments/:comment_id/reports(.:format)     reports#create
+      # new_comment_report_path GET  /comments/:comment_id/reports/new(.:format) reports#new
+      resources :reports, only: %i[new create]
+    end
   end
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
