c9b7a4b63ba1aed6af6d5e9ffcc869ebd8ede039
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2023-07-08 20:01:15 +0900

    current_shipping_addressesコントローラupdate アクション作成47.1

--- app/controllers/current_shipping_addresses_controller.rb
+++ app/controllers/current_shipping_addresses_controller.rb
@@ -1,4 +1,18 @@
 class CurrentShippingAddressesController < ApplicationController
+  before_action :authenticate_user!
+
   def update
+    @item = Item.find(params[:item_id])
+
+    @current_shipping_address = CurrentShippingAddress.find_or_initialize_by(
+      user_id: current_user.id
+      )
+
+    if @current_shipping_address.update(shipping_address_id: params[:shipping_address_id])
+      redirect_to item_shipping_addresses_path(@item), notice: '配送先を変更しました'
+    else
+      flash.now[:alert] = '配送先の変更に失敗しました'
+      render "shipping_addresses/index"
+    end
   end
 end
