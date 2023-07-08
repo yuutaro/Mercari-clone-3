7b207558146d8c62e91113badc438ca7b6b1ed0e
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-08 16:04:13 +0900

    42.12~42.16 Stripe支払情報保存用モデルの作成

--- app/models/stripe_customer.rb
+++ app/models/stripe_customer.rb
@@ -1,13 +1,15 @@
 class StripeCustomer < ApplicationRecord
   belongs_to :user
 
+  has_many :stripe_payments, dependent: :destroy
+  
   validates :customer_id, presence: true
   validates :customer_id, uniqueness: true
 
   before_validation :create_stripe_customer, if: :new_record?    
   
   private
-  
+
   def create_stripe_customer
     stripe_customer = Stripe::Customer.create(metadata: { user_id: user.id → })
     self.customer_id = stripe_customer.id
