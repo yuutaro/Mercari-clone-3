88ba23b0159e53478a5fbaa66c2074c9376f2459
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2023-07-08 18:20:11 +0900

    ShippingAddressモデルメソッド追加

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -9,9 +9,21 @@ class ShippingAddress < ApplicationRecord
   validates :given_name,         presence: :true, format: { with: ZENKAKU_REGEX, message: '全角文字を使用してください' }, allow_blank: true
   validates :family_name_kana,   presence: :true, format: { with: ZENKAKU_KANA_REGEX, message: '全角カタカナを使用してください' }, allow_blank: true
   validates :given_name_kana,    presence: :true, format: { with: ZENKAKU_KANA_REGEX, message: '全角カタカナを使用してください' }, allow_blank: true
-  validates :postal_code,        presence: :true
+  validates :postal_code,        presence: :true  # 郵便番号
   validates :city,               presence: :true
   validates :line,               presence: :true
   validates :building_name,      presence: :true
   validates :phone_number,       presence: :true
+
+  def full_name
+    "#{family_name} #{given_name}"
+  end
+
+  def display_postal_code
+    postal_code.to_s.insert(3, '-')
+  end
+
+  def address
+    "#{prefecture.name} #{city} #{line} #{building_name}"
+  end
 end
