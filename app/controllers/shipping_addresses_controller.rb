04b1fef64c41da389675ac05d5ab136ddb578a20
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-09 13:44:50 +0900

    45.10 shipping_addressコントローラ createアクション実装

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,6 +1,6 @@
 class ShippingAddressesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: %i[new]
+  before_action :set_item, only: %i[new create]
   def index
   end
 
@@ -9,6 +9,14 @@ class ShippingAddressesController < ApplicationController
   end
 
   def create
+    @shipping_address = current_user.shipping_addresses.build(shipping_address_params)
+
+    if @shipping_address.save
+      redirect_to item_shipping_addresses_path(@item), notice: "配送先住所の登録に成功しました"
+    else
+      flash.now.alert = "配送先住所の登録に失敗しました"
+      render :new
+    end
   end
 
   def edit
@@ -23,5 +31,20 @@ class ShippingAddressesController < ApplicationController
   private
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(
+      :family_name,
+      :given_name,
+      :family_name_kana,
+      :given_name_kana,
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :line,
+      :building_name,
+      :phone_number
+    )
   end
 end
