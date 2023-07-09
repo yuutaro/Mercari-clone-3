2add82ecb8b9488771cef14d55cf4dca1356aa0b
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-09 13:28:53 +0900

    45.6~45.7 shipping_addressコントローラ newアクション実装

---
+++ app/controllers/shipping_addresses_controller.rb
@@ -0,0 +1,27 @@
+class ShippingAddressesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: %i[new]
+  def index
+  end
+
+  def new
+    @shipping_address = current_user.shipping_addresses.build
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
