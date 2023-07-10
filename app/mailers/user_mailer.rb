20a4d2669a807f6393043c52c3cc1475e40e4d14
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-07-10 12:43:39 +0900

    Userメイラーnotify_commentメソッド text 作成  ch53.2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,15 @@ class UserMailer < ApplicationMailer
   #
   def notify_ordered(order)
     @order = order
+    
     mail to: @order.user.email,
        subject: "【URIKAI】#{@order.item.name}の発送をお願いします"
+  end
+
+  def notify_comment(comment)
+    @comment = comment
+
+    mail to: @comment.item.user.email,
+        subject: "【URIKAI】#{comment.item.name}にコメントがありました。"
   end
 end
