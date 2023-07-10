dd9a37fe8124fc81de6a6f4f8b012943ae0ee223
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-07-10 10:52:20 +0900

    orderコントローラーcreateアクション作成 ch51.8

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,13 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(user: current_user, item: @item)
+    if @order.pay!
+      redirect_to root_path, notice: "商品を購入しました"
+    else
+      flash.now[:alert] = "商品の購入に失敗しました"
+      render :new
+    end
   end
 
   private
