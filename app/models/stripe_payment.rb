8a7ef1866dbc729ea990429e4d822dfa970b53e5
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-10 16:17:39 +0900

    50.7~50.8 orders newView 支払方法の修正

--- app/models/stripe_payment.rb
+++ app/models/stripe_payment.rb
@@ -2,10 +2,17 @@ class StripePayment < ApplicationRecord
   belongs_to :stripe_customer
 
   has_one :current_stripe_payment, dependent: :destroy
-  
+
   validates :payment_id, presence: true
   validates :brand, presence: true
   validates :exp_month, presence: true
   validates :exp_year, presence: true
   validates :last4, presence: true
+
+  def expired?
+    Date
+      .new(exp_year, exp_month)
+      .end_of_month
+      .past?
+  end
 end
