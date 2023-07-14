6cfa421926f517cbbce275ebb659b317859c967d
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2023-07-14 17:13:23 +0900

    payer_evaluationコントローラcreateアクション作成 ch60.11

--- app/controllers/payer_evaluations_controller.rb
+++ app/controllers/payer_evaluations_controller.rb
@@ -3,9 +3,27 @@ class PayerEvaluationsController < ApplicationController
   before_action :set_order
 
   def create
+    @order.build_payer_evaluation(payer_evaluation_params)
+    @order.status = :received
+
+    if @order.save
+      redirect_to order_path(@order), notice: '評価しました'
+    else
+      @order.restore.status!
+      flash.now.alert = '評価に失敗しました'
+      render 'orders/show'
+    end
   end
 
   private
+
+  def payer_evaluation_params
+    params.require(:payer_evaluation).permit(
+      :received,
+      :good,
+      :comment
+      )
+  end
 
   def set_order
     @order = Order.find(params[:order_id])
