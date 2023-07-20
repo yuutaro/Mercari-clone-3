3c378f925724abaa1b035637d6c8a46a9ad0be4c
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-20 15:28:03 +0900

    70.5 マイページ itemsコントローラ indexアクション実装

--- app/controllers/mypage/items_controller.rb
+++ app/controllers/mypage/items_controller.rb
@@ -1,4 +1,16 @@
 class Mypage::ItemsController < ApplicationController
+  #layoutファイルをmypageに変更
+  layout "mypage"
+
+  before_action :authenticate_user!
+
+  #注文が完了してない商品を取り出すメソッド
   def index
+    #orderのidが存在しないレコードを取得
+    @items =
+      current_user
+        .items
+        .left_joins(:order)
+        .where(order: { id: nil })
   end
 end
