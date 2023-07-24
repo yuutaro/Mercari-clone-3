1a958a6f474b5bbf02ecdfc6da25f269efcd8b70
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-24 23:42:54 +0900

    73.1~73.6 お知らせ機能 Notificationsモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_14_075905) do
+ActiveRecord::Schema.define(version: 2023_07_24_142737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,13 @@ ActiveRecord::Schema.define(version: 2023_07_14_075905) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["order_id"], name: "index_messages_on_order_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|
