d9fa13e347d7ac2af60f34fae5064d3b568a7883
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-28 14:07:44 +0900

    75.1~75.5 フォロー機能 Relationshipモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,20 @@ class User < ApplicationRecord
   has_many :reports, dependent: :destroy
   has_many :shipping_addresses, dependent: :destroy
 
+  has_many :active_relationships,
+    class_name: "Relationship",
+    foreign_key: "follower_id",
+    dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships,
+    class_name: "Relationship",
+    foreign_key: "followed_id",
+    dependent: :destroy
+    
+  has_many :followers, through: :passive_relationships, source: :follower
+
   enum gender: {
     unanswered: 0,
     male: 1,
