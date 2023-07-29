2efff872aef28dd36e8adf36364e9afa8ff5595d
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-29 15:02:55 +0900

    75.13~75.15 フォロー解除用 destroyアクション実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,11 +4,16 @@ class RelationshipsController < ApplicationController
   def create
     #ユーザーIDを探し、ユーザーレコード代入
     user = User.find(params[:user_id])
-    #ユーザー間の関連付け
+    #followメソッドを用いてユーザー間の関連付け
     current_user.follow(user)
     redirect_to user_path(user), notice: "フォローに成功しました"
   end
 
   def destroy
+    #ユーザーIDを探し、ユーザーレコード代入
+    user = User.find(params[:user_id])
+    #unfollowメソッドを用いてユーザー間の関連付け
+    current_user.unfollow(user)
+    redirect_to user_path(user), notice: "フォロー解除に成功しました"
   end
 end
