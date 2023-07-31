b87b3778a0f53cae80be787bedf5171ea147af61
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2023-07-31 12:25:22 +0900

    76.14~76.18 Profileコントローラ updateアクション実装

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -3,4 +3,23 @@ class MypageController < ApplicationController
   
   def index
   end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user), notice: "プロフィールの更新に成功しました"
+    else
+      flash.now.alert = "プロフィールの更新に失敗しました"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    #profile_attributes内に含まれ、許可したいパラメータを定義
+    params.require(:user).permit(
+      :nickname,
+      profile_attributes: %i[avatar avatar_cache introduction]
+    )
+  end
 end
